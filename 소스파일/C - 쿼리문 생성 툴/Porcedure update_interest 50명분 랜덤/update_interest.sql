4449de9197f94e5c9d9377ad94c7dfb585f46e4e
goal6722 <126537110+goal6722@users.noreply.github.com>
2023-04-03 11:58:23 +0900

    관심사 업데이트
    관심사 업데이트 C 코드와 프로시저 입니다.

---
+++ 소스파일/C - 쿼리문 생성 툴/Porcedure update_interest 50명분 랜덤/update_interest.sql
@@ -0,0 +1,250 @@
+BEGIN
+update_interest('studious_123', '9');
+END;
+
+/
+BEGIN
+update_interest('smartgirl_456', '9');
+END;
+
+/
+BEGIN
+update_interest('bookworm_789', '4');
+END;
+
+/
+BEGIN
+update_interest('genius_kid123', '1');
+END;
+
+/
+BEGIN
+update_interest('studysquad_246', '2');
+END;
+
+/
+BEGIN
+update_interest('mathwhiz_135', '4');
+END;
+
+/
+BEGIN
+update_interest('englishpro_567', '6');
+END;
+
+/
+BEGIN
+update_interest('sciencegeek_890', '3');
+END;
+
+/
+BEGIN
+update_interest('studybuddy_1234', '2');
+END;
+
+/
+BEGIN
+update_interest('historybuff_5678', '9');
+END;
+
+/
+BEGIN
+update_interest('pinkunicorn123', '8');
+END;
+
+/
+BEGIN
+update_interest('galaxytraveler22', '4');
+END;
+
+/
+BEGIN
+update_interest('bookworm_librarian', '8');
+END;
+
+/
+BEGIN
+update_interest('nightowl_student', '3');
+END;
+
+/
+BEGIN
+update_interest('sunnybeachlover', '9');
+END;
+
+/
+BEGIN
+update_interest('techgeek2022', '9');
+END;
+
+/
+BEGIN
+update_interest('musicjunkie88', '6');
+END;
+
+/
+BEGIN
+update_interest('sweettoothbaker', '5');
+END;
+
+/
+BEGIN
+update_interest('yogafitnessfan', '2');
+END;
+
+/
+BEGIN
+update_interest('adventureseeker21', '7');
+END;
+
+/
+BEGIN
+update_interest('candy_lover', '1');
+END;
+
+/
+BEGIN
+update_interest('blue_moon', '5');
+END;
+
+/
+BEGIN
+update_interest('star_dreamer', '1');
+END;
+
+/
+BEGIN
+update_interest('velvet_echo', '4');
+END;
+
+/
+BEGIN
+update_interest('mystic_garden', '7');
+END;
+
+/
+BEGIN
+update_interest('silver_wings', '6');
+END;
+
+/
+BEGIN
+update_interest('midnight_rider', '6');
+END;
+
+/
+BEGIN
+update_interest('honey_bloom', '2');
+END;
+
+/
+BEGIN
+update_interest('ocean_whisper', '1');
+END;
+
+/
+BEGIN
+update_interest('golden_glory', '6');
+END;
+
+/
+BEGIN
+update_interest('happy_day123', '7');
+END;
+
+/
+BEGIN
+update_interest('future_plan567', '9');
+END;
+
+/
+BEGIN
+update_interest('study_hard789', '7');
+END;
+
+/
+BEGIN
+update_interest('peaceful_mind111', '5');
+END;
+
+/
+BEGIN
+update_interest('success_story555', '8');
+END;
+
+/
+BEGIN
+update_interest('learning_way222', '5');
+END;
+
+/
+BEGIN
+update_interest('knowledge_hub999', '7');
+END;
+
+/
+BEGIN
+update_interest('growth_mindset333', '2');
+END;
+
+/
+BEGIN
+update_interest('challenge_accepted4', '4');
+END;
+
+/
+BEGIN
+update_interest('explore_more777', '5');
+END;
+
+/
+BEGIN
+update_interest('happyhiker33', '2');
+END;
+
+/
+BEGIN
+update_interest('coffeeaddict99', '7');
+END;
+
+/
+BEGIN
+update_interest('bookwormgal', '1');
+END;
+
+/
+BEGIN
+update_interest('musicjunkie22', '5');
+END;
+
+/
+BEGIN
+update_interest('wanderlustguru', '8');
+END;
+
+/
+BEGIN
+update_interest('doglover2023', '1');
+END;
+
+/
+BEGIN
+update_interest('fitnessfreak88', '9');
+END;
+
+/
+BEGIN
+update_interest('plantmama77', '2');
+END;
+
+/
+BEGIN
+update_interest('adventureseeker10', '9');
+END;
+
+/
+BEGIN
+update_interest('foodieforever999', '8');
+END;
+
+/
