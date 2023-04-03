164f0e1d03ad9ac7df6a5d10ff13d223d878b89e
Holorifle <shinemper@gmail.com>
2023-04-03 10:17:00 +0900

    코드번호 0부터 시작하게 수정

--- 소스파일/학력테이블과 내용/education_level_table.sql
+++ 소스파일/학력테이블과 내용/education_level_table.sql
@@ -3,8 +3,8 @@ CREATE TABLE education_level (
   education_level_name VARCHAR2(50) NOT NULL -- 학력수준 이름
 );
 
-INSERT INTO education_level (id, name) VALUES (1, '중학교 졸업');
-INSERT INTO education_level (id, name) VALUES (2, '고등학교 졸업');
-INSERT INTO education_level (id, name) VALUES (3, '전문대학 졸업');
-INSERT INTO education_level (id, name) VALUES (4, '대학교 졸업');
-INSERT INTO education_level (id, name) VALUES (5, '대학원 졸업 이상');
+INSERT INTO education_level (id, name) VALUES (0, '중학교 졸업');
+INSERT INTO education_level (id, name) VALUES (1, '고등학교 졸업');
+INSERT INTO education_level (id, name) VALUES (2, '전문대학 졸업');
+INSERT INTO education_level (id, name) VALUES (3, '대학교 졸업');
+INSERT INTO education_level (id, name) VALUES (4, '대학원 졸업 이상');
